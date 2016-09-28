86d0683c1e166eb4ea49baec2d5761077a37c35b
BSLizon <bslizon@gmail.com>
2016-09-28 12:06:26 +0800

    fix

--- Assets/Script/Common/Entity.cs
+++ Assets/Script/Common/Entity.cs
@@ -4,5 +4,21 @@ using System.Collections.Generic;
 
 public class Entity
 {
-    
+    Dictionary<PropDef, System.Object> propDic = new Dictionary<PropDef, System.Object>();
+
+    public bool GetProp(PropDef prop, out System.Object obj)
+    {
+        bool ret;
+        ret = propDic.TryGetValue(prop, out obj);
+        if (!ret)
+        {
+            Debug.LogWarning("Get Null Prop");
+        }
+        return ret;
+    }
+
+    public void SetProp(PropDef prop, System.Object obj)
+    {
+        propDic[prop] = obj;
+    }
 }
---
+++ Assets/Script/Common/PropDef.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PropDef
+{
+    None,
+    HP,         //float
+    MP,         //float
+    Money,      //int
+    END         
+}
